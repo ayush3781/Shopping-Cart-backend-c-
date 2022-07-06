d26d72f90f29f28728bd86db8e4487761c117810
Ayush aryan <75092320+ayush3781@users.noreply.github.com>
2022-07-06 17:20:57 +0530

    Add files via upload

---
+++ cart_main.cpp
@@ -0,0 +1,88 @@
+#include<iostream>
+#include<vector>
+#include"model.h"
+using namespace std;
+
+vector<Product> allProducts={
+    Product(1,"apple",26),
+    Product(3,"mango",26),
+    Product(2,"guava",26),
+    Product(5,"banana",26),
+    Product(4,"strawberry",26),
+    Product(1,"pineapple",26),
+};
+Product* chooseProduct(){
+    string productList;
+    cout<<"Available Products "<<endl;
+    for(auto product : allProducts)
+    {
+        productList.append(product.getDisplayName());
+    }
+    cout<< productList<<endl;
+    cout<< "-------------------"<<endl;
+    string choice;
+    cin>>choice;
+    for(int i=0;i<allProducts.size();i++)
+    {
+        if(allProducts[i].getShortName()==choice)
+        {
+            return &allProducts[i];
+        }
+    }
+    cout<<"Product not found"<<endl;
+    return NULL;
+}
+    bool checkout(Cart &cart){
+        if(cart.isEmpty())
+        {
+            return false;
+        }
+        int total = cart.getTotal();
+        cout<<"Pay in Cash";
+        int paid;
+        cin>>paid;
+        if(paid>=total){
+            cout<<"change"<<paid - total << endl;
+            cout<<"Successfull checkout";
+            return true;
+
+        }
+        else{
+            cout<<"not enough cash";
+            return false;
+
+        }
+
+    }
+
+
+int main()
+{
+    char action;
+    Cart cart;
+    while(true)
+    {
+        cout<<"Select an action - (a) dd item, (v)iew cart, (c)heckout"<<endl;
+        cin>> action;
+        if(action ='a'){
+            Product * product =chooseProduct();
+            if(product!=NULL){
+                cout<<"Added to the cart"<<'\t'<<product->getDisplayName()<<endl;
+                cart.addProduct(*product);
+            }
+        }
+        else if(action=='v')
+        {
+            cout<<"----------------------"<<endl;
+            cout<<cart.viewCart();
+            cout<<"----------------------"<<endl;
+        }
+        else{
+            cart.viewCart();
+            if(checkout(cart)){
+                break;
+            }
+        }
+    }
+    return 0;
+}
